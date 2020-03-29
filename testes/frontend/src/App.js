8fe4b053b91b0f1d177fb2575f51ee0a3012530f
Aarubi <arthurleitaobarbosa@hotmail.com>
2020-03-29 17:49:46 -0300

    Front-end do app

--- testes/frontend/src/App.js
+++ testes/frontend/src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './global.css';
+import Routes from './routes';
+import { Route } from 'react-router-dom';
+
+//import Header from './Header';
+
+
+//JSX (JavaScript XML)
+//componente do React: uma funcao que retorna html
+//Estado: atributos repassados para componentes
+
 
 function App() {
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes />
   );
 }
 
---
+++ testes/frontend/src/Header.js
@@ -0,0 +1,10 @@
+//Não usado na aplicação. so deixei pq ta muito bonitinho
+import React from 'react';
+
+export default function Header({ children }){
+    return(
+        <header>
+            <h1>{children}</h1>
+        </header>
+    );
+}
